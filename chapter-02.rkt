a4e366798320c61a5e3c3412854035985d41731b
Ray Xu <megrxu@gmail.com>
2020-02-17 21:52:13 +0800

    chapter 02: 2.6

--- chapter-02.rkt
+++ chapter-02.rkt
@@ -1,35 +1,141 @@
 #lang racket
 
-(define (acc op init seq)
-  (if (null? seq)
-      init
-      (op (car seq) (acc op init (cdr seq)))))
+;; 2.1.1 aux
 
-(acc + 0 '(1 2 3 4 5))
-(acc * 1 '(1 2 3 4 5))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
 
-(define (accn op init seqs)
-  (if (null? (car seqs))
-      '()
-      (cons (acc op init (map car seqs))
-            (accn op init (map cdr seqs)))))
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (denom x) (numer y))
+     (* (numer x) (denom y))))
 
-(accn + 0 (list (list 1 2 3) (list 2 3 4)))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (displayln (denom x)))
 
-;; 2.37 Matrix Ops
+;; 2.1
+(define (make-rat n d) (let [(g (gcd n d))]
+                         (if (positive? d)
+                             (cons (/ n g) (/ d g))
+                             (cons (- (/ n g)) (- (/ d g))))))
+(print-rat
+ (mul-rat
+  (make-rat -2 3)
+  (make-rat 1 -4))) ;; -2/3 * 1/(-4) = 1/6
 
-(define mat
-  (list (list 1 0 0 0)
-        (list 0 1 0 0)
-        (list 0 0 1 0)
-        (list 0 0 0 1)))
+;; 2.2
 
-(define (dot-product v w)
-  (acc + 0 (map * v w)))
+(define (make-segment p1 p2)
+  (cons p1 p2))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cdr segment))
+(define (make-point x y)
+  (cons x y))
+(define (x-point point)
+  (car point))
+(define (y-point point)
+  (cdr point))
 
-(dot-product (list 1 2 3 4) (list 2 3 4 5))
+(define (midpoint-segment segment)
+  (define (average x y)
+    (/ (+ x y) 2.0))
+  (make-point
+   (average (x-point (start-segment segment))
+            (x-point (end-segment segment)))
+   (average (y-point (start-segment segment))
+            (y-point (end-segment segment)))))
 
-(define (mat-*-vec m v)
-  (map (lambda x (dot-product (car x) v)) m))
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ", ")
+  (display (y-point p))
+  (displayln ")"))
 
-(mat-*-vec mat (list 1 1 1 1))
+(define p1 (make-point 1.0 4.0))
+(define p2 (make-point 7.0 -2.0))
+(define s0 (make-segment p1 p2))
+(print-point (midpoint-segment s0))
+
+
+;; 2.3
+(define (make-rect point rect-length rect-width)
+  (cons point (cons rect-length rect-width)))
+(define (rect-length rect)
+  (cadr rect))
+(define (rect-width rect)
+  (cddr rect))
+(define (rect-aera rect)
+  (* (rect-length rect)
+     (rect-width rect)))
+(define (rect-perimeter rect)
+  (* 2 (+ (rect-length rect)
+          (rect-width rect))))
+(define rect (make-rect (make-point 2.0 3.0)
+                           9
+                           4.0))
+(rect-perimeter rect)
+(rect-aera rect)
+
+;; 2.4
+
+(define (playground-2-4)
+  (define (cons x y)
+    (lambda (m) (m x y)))
+  (define (car z)
+    (z (lambda (p q) p)))
+  (define (cdr z)
+    (z (lambda (p q) q)))
+  (displayln (car (cons 1 2)))
+  (displayln (cdr (cons 1 2))))
+(playground-2-4)
+
+;; 2.5
+
+(define (playground-2-5)
+  (define (cons a b)
+    (* (expt 2 a)
+       (expt 3 b)))
+  (define (car z)
+    (cond ((= (modulo z 2) 0) (+ (car (/ z 2)) 1))
+          (else 0)))
+  (define (cdr z)
+    (cond ((= (modulo z 3) 0) (+ (cdr (/ z 3)) 1))
+          (else 0)))
+  (displayln (cons 1 2))
+  (displayln (car (cons 1 2)))
+  (displayln (cdr (cons 1 2))))
+(playground-2-5)
+
+;; 2.6
+
+(define (playground-2-6)
+  (define zero (lambda (f) (lambda (x) x)))
+  (define (add-1 n)
+    (lambda (f) (lambda (x) (f ((n f) x)))))
+  (define one
+    (lambda (f) (lambda (x) (f x))))
+  (define two
+    (lambda (f) (lambda (x) (f (f x)))))
+  (define (plus x y)
+    (lambda (f) (lambda (x) ((x f) ((y f) x)))))
+  (display "Church"))
