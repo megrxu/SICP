8c6ee2b93b79690fd8a1b4e48dfc50ff9a887db2
Ray Xu <megrxu@gmail.com>
2020-02-18 21:40:14 +0800

    chapter 02: 2.10

--- chapter-02.rkt
+++ chapter-02.rkt
@@ -139,3 +139,42 @@
   (define (plus x y)
     (lambda (f) (lambda (x) ((x f) ((y f) x)))))
   (display "Church"))
+
+;; 2.7
+
+(define (make-interval a b) (cons a b))
+
+(define lower-bound car)
+(define upper-bound cdr)
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+(define (mul-interval x y)
+  (let [(p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y)))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+;; 2.8
+
+(define (sub-interval x y)
+  (let [(p1 (- (lower-bound x) (lower-bound y)))
+        (p2 (- (lower-bound x) (upper-bound y)))
+        (p3 (- (upper-bound x) (lower-bound y)))
+        (p4 (- (upper-bound x) (upper-bound y)))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+;; 2.10
+
+(define (div-interval x y)
+  (let [(yl (lower-bound y))
+        (yu (upper-bound y))]
+    (if (positive? (* yl yu))
+        (mul-interval x
+                      (make-interval (/ 1.0 yu)
+                                     (/ 1.0 yl)))
+        (error "Div failed."))))
+
